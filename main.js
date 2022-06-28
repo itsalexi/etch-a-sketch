f544c57d3cd0bf6fd01cd8bbfb57ef87e46fed94
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-06-28 09:03:44 +0800

    Created a removeCanvas function

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-// let gridSize = 16
-const canvas = document.querySelector("#canvas");
+let gridSize = 16;
+const mainDiv = document.querySelector(".main");
 
 function generateGrid(size) {
+  const canvas = document.createElement("div");
+  canvas.setAttribute("id", "canvas");
+  mainDiv.append(canvas);
   for (let i = 0; i < size * size; i++) {
     console.log("test");
     const pixel = document.createElement("div");
@@ -13,3 +16,10 @@ function generateGrid(size) {
 
   canvas.style.cssText = `grid-template-columns: repeat(${size}, 25px [col-start])`;
 }
+
+function removeCanvas() {
+  const canvas = document.querySelector("#canvas");
+  canvas.remove();
+}
+
+generateGrid(16);
